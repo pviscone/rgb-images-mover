5cd2871f3ef1667e353175bda08d392342d730b8
Piero Viscone <pieroviscone@gmail.com>
2020-04-10 20:00:40 +0200

    Sposta tutte le immagini presenti in una directory con determinata percentuale di pixel sul totale in un determinato range RGB in un altra directory

---
+++ rgb images mover.py
@@ -0,0 +1,35 @@
+import glob
+import os
+import PIL
+from PIL import Image
+
+path='/home/pviscone/Scrivania/WhatsApp Images'
+destination='/home/pviscone/Scrivania/bianche/'
+
+r_under_threshold=150
+r_up_threshold=255
+g_under_threshold=150
+g_up_threshold=255
+b_under_threshold=150
+b_up_threshold=255
+percentage_threshold=0.3
+
+for filename in glob.glob(os.path.join(path,'*.jpg')):
+    print(filename.split('/')[-1])
+    a=0
+    try:
+        im=Image.open(filename).convert('RGB')
+        pix=im.load()
+        w=im.size[0]
+        h=im.size[1]
+        for i in range(w):
+            for j in range(h):
+                if (pix[i,j][0]>r_under_threshold) and (pix[i,j][1]>g_under_threshold) and (pix[i,j][2]>b_under_threshold) and (pix[i,j][0]<r_up_threshold) and (pix[i,j][1]<g_up_threshold) and (pix[i,j][2]<b_up_threshold):
+                    a=a+1
+        if a/(w*h)>percentage_threshold:
+            print('    MOVED    ')
+            os.rename(filename,destination+filename.split('/')[-1])
+    except:
+        print ('!!ERROR!!')
+    print(' ')
+print('fine')
