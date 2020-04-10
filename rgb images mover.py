7f4128969f4eb043f26c1cbca1601cfa803bea07
pviscone <31247066+pviscone@users.noreply.github.com>
2020-04-10 20:28:42 +0200

    Update rgb images mover.py

--- rgb images mover.py
+++ rgb images mover.py
@@ -2,12 +2,10 @@ import glob
 import os
 from PIL import Image
 
-#estensione delle immagini, directory di partenza e di destinazione
 extension='.jpg'
 path='/home/pviscone/Scrivania/WhatsApp Images'
 destination='/home/pviscone/Scrivania/bianche/'
 
-#setting del range rgb e della threshold sulla percentuale
 r_under_threshold=150
 r_up_threshold=255
 g_under_threshold=150
@@ -16,8 +14,11 @@ b_under_threshold=150
 b_up_threshold=255
 percentage_threshold=0.3
 
+b=0
+c=0
 for filename in glob.glob(os.path.join(path,'*'+extension)):
-    print(filename.split('/')[-1])
+    b+=1
+    print('#'+str(b)+':  '+filename.split('/')[-1])
     a=0
     try:
         im=Image.open(filename).convert('RGB')
@@ -27,11 +28,15 @@ for filename in glob.glob(os.path.join(path,'*'+extension)):
         for i in range(w):
             for j in range(h):
                 if (pix[i,j][0]>r_under_threshold) and (pix[i,j][1]>g_under_threshold) and (pix[i,j][2]>b_under_threshold) and (pix[i,j][0]<r_up_threshold) and (pix[i,j][1]<g_up_threshold) and (pix[i,j][2]<b_up_threshold):
-                    a=a+1
+                    a+=1
+        print(str((a/(w*h))*100)+'%')
         if a/(w*h)>percentage_threshold:
-            print('    MOVED    ')
             os.rename(filename,destination+filename.split('/')[-1])
+            c+=1
+            print('                MOVED              ')
     except:
         print ('!!ERROR!!')
     print(' ')
-print('fine')
+print(' ')    
+print('         '+str(c)+'/'+str(b)+'immagini spostate           ')
+print('                         Fine                             ')
